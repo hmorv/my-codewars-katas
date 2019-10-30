d0cc7003d26abd7dfc45c1f101a6fb1804ff0bc7
Hugo Moragues <hugo.moragues@gmail.com>
2019-10-30 17:29:37 +0100

    Update app.js
    primera version completamente funcional (pasa todo los tests de codewars), falta refactor.

--- katas/prime-numbers-decompositions/app.js
+++ katas/prime-numbers-decompositions/app.js
@@ -3,6 +3,10 @@ function getAllPrimeFactors(n) {
         return [];
     }
 
+    if (n === 1) {
+        return [1];
+    }
+
     let result = [];
     let i = 2;
     let j = 2;
@@ -28,18 +32,56 @@ function getAllPrimeFactors(n) {
 }
 
 function getUniquePrimeFactorsWithCount(n) {
-    primes = getAllPrimeFactors(n);
+    let factors = [];
+    let powers = [];
+    let primes = getAllPrimeFactors(n);
+    let i = 0
+
     if (primes.length === 0) {
         return [[], []];
     }
+
+    while (i < primes.length) {
+        let actualElement = primes[i];
+        let counter = 0;
+        for (let j = i; j < primes.length; j++) {
+            if (primes[j] == actualElement) {
+                counter++;
+                i++;
+            } else {
+                i = j;
+                break;
+            }
+        }
+        factors.push(actualElement);
+        powers.push(counter);
+    }
+
+  return [factors, powers];
 }
 
 function getUniquePrimeFactorsWithProducts(n) { 
-    primes = getAllPrimeFactors(n);
-    if (primes.length === 0) {
-        return [];
+
+    let result = [];
+
+    primes = getUniquePrimeFactorsWithCount(n);
+    if (primes[0].length === 0 || primes[1].length === 0) {
+        return result;
     }
+
+    for(let i = 0; i < primes.length; i++) {
+        let product = primes[0][i] ** primes[1][i];
+        if (Number.isNaN(product)) {
+            product = 0;
+        } else {
+        result.push(product);
+        }
+    }
+
+    return result;
 }
 
 
-console.log(getAllPrimeFactors(100));
+console.log(getAllPrimeFactors(1));
+console.log(getUniquePrimeFactorsWithCount(1));
+console.log(getUniquePrimeFactorsWithProducts(1));
