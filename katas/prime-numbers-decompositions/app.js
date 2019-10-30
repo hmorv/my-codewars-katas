950a61b276443420ff840b1b60d4192b37d2c278
Hugo Moragues <hugo.moragues@gmail.com>
2019-10-30 09:40:13 +0100

    Update app.js
    getAllPrimeFactors ok, faltan las otras dos funciones.

--- katas/prime-numbers-decompositions/app.js
+++ katas/prime-numbers-decompositions/app.js
@@ -1,32 +1,45 @@
-function getAllPrimeFactors(n) { 
+function getAllPrimeFactors(n) {
+    if (isNaN(n) || n <= 0 || (! Number.isInteger(n))) {
+        return [];
+    }
+
     let result = [];
+    let i = 2;
+    let j = 2;
+    let number = n;
 
-    for (let i = 2; i < n; i++) {
-        let isPrime;
-        if (n % i === 0) {
-            isPrime = true;
-            for (let j = 2; j <= i; j++) {
-                //isPrime = (i % j === 0) ? false : true;
-                if (i % j === 0) {
-                    isPrime == false;
-                }
-            }
-        }
-
-        if (isPrime == true) {
+    for (; i <= number; i++) {
+        let isPrime = number % i === 0;
+        if (isPrime) {
             result.push(i);
+            number /= i;
+        }
+        while (isPrime) {
+            if (number % i === 0) {
+                result.push(i);
+                number /= i;
+            } else {
+                isPrime = false;
+            }
         }
     }
 
     return result;
-
 }
 
-function getUniquePrimeFactorsWithCount(n) { 
+function getUniquePrimeFactorsWithCount(n) {
+    primes = getAllPrimeFactors(n);
+    if (primes.length === 0) {
+        return [[], []];
+    }
 }
 
 function getUniquePrimeFactorsWithProducts(n) { 
+    primes = getAllPrimeFactors(n);
+    if (primes.length === 0) {
+        return [];
+    }
 }
 
 
-console.log(getAllPrimeFactors(40));
+console.log(getAllPrimeFactors(100));
