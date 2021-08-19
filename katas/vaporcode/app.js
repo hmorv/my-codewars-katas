91b63b2d2aea5d15cdce807f52607a9cebbc7b34
Hugo Moragues <hugo.moragues@gmail.com>
2021-08-19 17:28:15 +0200

    minor

--- katas/vaporcode/app.js
+++ katas/vaporcode/app.js
@@ -10,16 +10,3 @@ function vaporcode(string) {
 const test = "Esto es una prueba";
 
 console.log(vaporcode(test));
-
-
-// function vaporcode(string) {
-//     return string
-//     .replace(/\s+/g, '')
-//     .toUpperCase()
-//     .split('')
-//     //.map(char => `${' '+char+' '}`)
-//     .join(',')
-//     .replace(/,/g, '  ')
-//     console.log(salida)
-    
-// }
